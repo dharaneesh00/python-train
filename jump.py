d31b060e5fa8dad692d0d10c8483a001c06f1016
dharaneesh00 <rmdharaneeshh@gmail.com>
2025-07-23 19:33:09 +0530

    vanten mame

---
+++ jump.py
@@ -0,0 +1,10 @@
+score=int(input())
+divide=(score/10
+        )
+name=input("Enter your name")
+department=input("Enter your department"
+                 "")
+print(divide)
+print(name)
+print(department)
+
